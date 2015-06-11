d64dd491217f2437e82cb7e19d753bccb84ed742
lyncsta <lynctekura@gmail.com>
2015-06-11 18:10:30 +1200

    Update mini.user.js

--- mini.user.js
+++ mini.user.js
@@ -1516,3 +1516,123 @@ function DrawMinimap(oc, cells) {
         }
     }
 })(window, jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ==UserScript==
+// @name         Virus Shortcut
+// @version      1.0
+// @description  Press F to throw 7 masses instead of 1,for green cells
+// @author       Daniel
+// @match        http://agar.io/
+// @require      https://code.jquery.com/jquery-latest.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/canvasjs/1.4.1/canvas.min.js
+// @run-at       document-start
+// @grant        none
+// @namespace http://your.homepage/
+// ==/UserScript==
+
+function pressW() {
+	var oEvent = document.createEvent('KeyboardEvent');
+	var k = 87;
+	// Chromium Hack
+	Object.defineProperty(oEvent, 'keyCode', {
+	            get : function() {
+	                return this.keyCodeVal;
+	            }
+	});     
+	Object.defineProperty(oEvent, 'which', {
+	            get : function() {
+	                return this.keyCodeVal;
+	            }
+	});     
+
+	if (oEvent.initKeyboardEvent) {
+	    oEvent.initKeyboardEvent("keydown", true, true, document.defaultView, false, false, false, false, k, k);
+	} else {
+	    oEvent.initKeyEvent("keydown", true, true, document.defaultView, false, false, false, false, k, 0);
+	}
+
+	oEvent.keyCodeVal = k;
+
+	if (oEvent.keyCode !== k) {
+	    console.log("keyCode mismatch " + oEvent.keyCode + "(" + oEvent.which + ")");
+	}
+	document.dispatchEvent(oEvent);
+
+	var oEvent = document.createEvent('KeyboardEvent');
+	// Chromium Hack
+	Object.defineProperty(oEvent, 'keyCode', {
+	            get : function() {
+	                return this.keyCodeVal;
+	            }
+	});     
+	Object.defineProperty(oEvent, 'which', {
+	            get : function() {
+	                return this.keyCodeVal;
+	            }
+	});     
+
+	if (oEvent.initKeyboardEvent) {
+	    oEvent.initKeyboardEvent("keyup", true, true, document.defaultView, false, false, false, false, k, k);
+	} else {
+	    oEvent.initKeyEvent("keyup", true, true, document.defaultView, false, false, false, false, k, 0);
+	}
+
+	oEvent.keyCodeVal = k;
+
+	if (oEvent.keyCode !== k) {
+	    console.log("keyCode mismatch " + oEvent.keyCode + "(" + oEvent.which + ")");
+	}	
+	document.dispatchEvent(oEvent);
+}
+window.pressW = pressW;
+document.onkeypress = function(e) {
+	e = e || window.event;
+	if (e.keyCode == 102) {
+		for (var i = 0; i<7; i++) {
+			setTimeout(pressW, i * 80);
+		}
+	} else if (e.keyCode == 103) {
+		for (var i = 0; i<50; i++) {
+			setTimeout(pressW, i * 40);
+		}
+	} else if (e.keyCode == 104) {
+		for (var i = 0; i<400; i++) {
+			setTimeout(pressW, i * 5);
+		}
+	}
+}
